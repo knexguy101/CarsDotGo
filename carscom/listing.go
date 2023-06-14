287acbca4244937de2001ece5f1a993eebaebce9
Chris <chris.falb@hotmail.com>
2023-06-13 21:43:32 -0500

    init

---
+++ carscom/listing.go
@@ -0,0 +1,37 @@
+package carscom
+
+import "github.com/PuerkitoBio/goquery"
+
+type Listing struct {
+	Title        string
+	Miles        string
+	Link         string
+	PrimaryPrice string
+	PriceDrop    string
+	Carfax       string
+	DealerName   string
+	MilesFrom    string
+	DealLabel    string
+	DealPrice    string
+}
+
+func ParseListings(doc *goquery.Document) []Listing {
+	var listings []Listing
+	doc.Find(".vehicle-card   ").Each(func(i int, selection *goquery.Selection) {
+		l := Listing{}
+
+		l.Title = selection.Find(".title").Text()
+		l.Miles = selection.Find(".mileage").Text()
+		l.Link = selection.Find(".vehicle-card-link").AttrOr("href", "")
+		l.PrimaryPrice = selection.Find(".primary-price").Text()
+		l.PriceDrop = selection.Find(".secondary-price").Text()
+		l.Carfax = selection.Find(".sds-link--ext").AttrOr("href", "")
+		l.DealerName = selection.Find(".dealer-name > strong").Text()
+		l.MilesFrom = selection.Find(".miles-from ").Text()
+		l.DealLabel = selection.Find(".sds-badge__label").Text()
+		l.DealPrice = selection.Find(".sds-badge--price-savings").Text()
+
+		listings = append(listings, l)
+	})
+	return listings
+}
